93412dada5198f5a77fd376e88f6e37e2c2da2ec
Cai Silva <crs_caique@hotmail.com>
2025-03-30 19:08:24 +1300

    Logout user

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,5 +1,5 @@
 # users/urls.py
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
@@ -10,6 +10,6 @@ from .views import LoginView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    # path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     ]
 
